4a8b1cf20ba6f8308a281e6979f8b44f8763ddca
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-10-31 13:33:53 -0700

    Created pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def Look
+  end
+  def Become
+  end
+  def How
+  end
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Belluga::Application.routes.draw do
+  root "pages#home"
+  get "look" => "pages#look"
+  get "become" => "pages#become"
+  get "how" => "pages#how"
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
