e51b2616c43abe6c47ca3d4a94449ee91e40a00a
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-11-01 19:29:05 -0700

    Photos 3

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,3 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "50x50>" }, :default_url => "default.jpg"
+  has_attached_file :image, :styles => { :medium => "100x100>", :thumb => "50x50>" }, :default_url => "default.jpg"
 end
